4440cb22fef507aaa8100968600995a97f3188c6
takumiwish <takutaku19961027@icloud.com>
2019-07-14 15:29:37 +0900

    made a simple bot

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
-print("hello")
+import discord
+
+TOKEN = ''
+with open('discord_token.txt', 'r') as f:
+    TOKEN = f.read()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        # メッセージ送信者がBotだった場合は無視する
+        if message.author.bot:
+            return
+        # 「/neko」と発言したら「にゃーん」が返る処理
+        if message.content == '/neko':
+            await message.channel.send('にゃーん')
+
+client = MyClient()
+client.run(TOKEN)
