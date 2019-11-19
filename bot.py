af5f35ecf28b40afa28375306813dbb343dad741
masa <6446790579@edu.k.u-tokyo.ac.jp>
2019-11-19 16:08:16 +0900

    MyClient.schedule関数の実装。print文のみを実行するtestfuncがスケジュール実行出来ることまで確認。

--- bot.py
+++ bot.py
@@ -32,7 +32,13 @@ class MyClient(discord.Client):
                 # 602776246673473565 make_test
                 # 603398450352226306 シート1
                 # 633598972510076938 nadchu_test
-        #asyncio.ensure_future(self.regular_check(60)) #定期実行タスクはバグっているので後で直す
+            self.schedule(1,self.testfunc,2,3)
+
+    def testfunc(self, a, b):
+        print(a+b)
+
+    def schedule(self, time, callback, *args):
+        self.loop.call_later(time, callback, *args)
 
     async def on_message(self, message):
         if   message.content.startswith("kill"): #debug botを停止
@@ -45,32 +51,26 @@ class MyClient(discord.Client):
                 print(k,":",v)
         elif message.content.startswith("exec"): #debug 任意コードの実行
             #用例 "exec print("Hello world!")"
-            if len(message.content)>=5:
+            if len(message.content)>5:
                 toexec=message.content[5:]
                 print(f"> exec({toexec})")
                 exec(toexec)
         elif message.content.startswith("print"): #debug 変数名等を指定してprint
             #用例 "print compe_mgr.uids"
-            if len(message.content)>=5:
+            if len(message.content)>5:
                 toeval=message.content[5:]
                 print(f"> print({toeval})")
                 toprint=eval(toeval)
                 print(toprint)
         elif message.content.startswith("send"): #debug 変数名等を指定してチャンネルに投下してもらう
             #用例 "send compe_mgr.uids"
-            if len(message.content)>=5:
+            if len(message.content)>5:
                 toeval=message.content[5:]
-                await message.channel.send(f"> print({toeval})")
+                await message.channel.send(f"> send({toeval})")
                 tosend=eval(toeval)
                 await message.channel.send(tosend)
         elif self.user in message.mentions and not message.author.bot: # botへ、他の人から為されたメンション
             await compe_mgr.get_compedata(message.channel.id).hub(message)
-
-    async def regular_check(self, interval): #on_readyから呼ばれる。ensure_futureによる定期実行、間隔はinterval秒。
-        while True:
-            await print("hello") #debug
-            #compe_mgr.check_schedule() こんな感じで時間経過に関した処理を実行？
-            await asyncio.sleep(interval)
 
     #拡張例
     async def on_member_join(self, member): #メンバーはChannelではなくGuildへ所属することに注意
